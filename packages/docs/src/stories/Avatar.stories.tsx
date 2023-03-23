8dcd53ee90f6b2ff423043e1bf2bc784e188491b
Thiago <ps-thiago@hotmail.com>
2023-03-22 21:22:00 -0300

    feat argTypes in stories

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/thiagopalhares77.png',
     alt: 'Thiago Palhares',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
